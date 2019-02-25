bee2a46dafed5b09304078ccd19c99ad9c23c9bc
Spencer Meredith <Smeredith1119@gmail.com>
2019-02-25 11:10:34 -0800

    added Links on Nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 
 
@@ -9,13 +9,13 @@ const Navigation = () => {
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          <a href="/">Home</a>
+          <Link to="/">Home</Link>
         </div>
         <div>
-          <a href="/about">About</a>
+          <Link to="/about">About</Link>
         </div>
         <div>
-          <a href="/contact">Contact</a>
+          <Link to="/contact">Contact</Link>
         </div>
       </div>
     </div>
